f6ac3f3885de5592d01151a9e59b85e02ee71267
unknown <ahedqaood19@gmail.com>
2022-03-30 03:00:15 +0300

    first commit

---
+++ attebdance_system.py
@@ -0,0 +1,79 @@
+from openpyxl import load_workbook
+import xlrd
+from datetime import date
+from datetime import datetime
+import time
+import serial
+import argparse
+ 
+#load excel file
+ids_file = "csv/registered_ids.xlsx"
+ids_wb = xlrd.open_workbook(ids_file)
+ids_sheet = ids_wb.sheet_by_index(0)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--port")
+args = parser.parse_args()
+print(args.port)
+ser = serial.Serial(args.port, '9600')
+isIdFound = False
+timer = time.time()
+
+try:
+    while True:
+        if (time.time() - timer) > 5:
+            isIdFound = False
+
+        message = ""
+        if not isIdFound:
+            ser.flushInput()
+            ser.flushOutput()
+            message = ser.readline().decode("utf-8")
+            id_attendee = message.rstrip().lstrip()
+            name_attendee = ""
+            role_attendee = ""
+
+            i=0
+            while True:
+                try:
+                    identifier = str(ids_sheet.cell_value(i, 0))
+                    name = str(ids_sheet.cell_value(i, 1))
+                    role = str(ids_sheet.cell_value(i, 2))
+                    if(identifier == id_attendee):
+                        name_attendee = name
+                        role_attendee = role
+                        isIdFound = True
+                        print(name+"  "+role)
+                        break
+                except IndexError:
+                    print('Not registered')
+                    break
+                i=i+1
+
+            if isIdFound:
+                j=0
+                while True:
+                    attendace_file  = "csv/attendace_report.xlsx"
+                    attendace_wb = load_workbook(filename=attendace_file)
+                    attendace_sheet = attendace_wb.active
+                    attendace_read_wb = xlrd.open_workbook(attendace_file)
+                    attendace_read_sheet = attendace_read_wb.sheet_by_index(0)
+                    try:
+                        Date = str(attendace_read_sheet.cell_value(j, 0))
+                    except IndexError:
+                        j=j+1
+                        attendace_sheet["A"+str(j)] = date.today()
+                        attendace_sheet["B"+str(j)] = datetime.now().strftime("%H:%M:%S")
+                        attendace_sheet["C"+str(j)] = name_attendee
+                        attendace_sheet["D"+str(j)] = role_attendee
+                        try:
+                            attendace_wb.save(filename=attendace_file)
+                        except IOError:
+                            print(attendace_file+" is open, please close file & try again!")
+                        
+                        timer = time.time()
+                        break
+                    j=j+1
+
+except KeyboardInterrupt:
+    pass
